759cb2267ff26410f16888aa22329bd0292531bd
Akash143333 <128797710+Akash143333@users.noreply.github.com>
2023-03-25 11:44:57 +0000

    added dynamic information

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, jsonify
 
-app=Flask(__name__)
+app = Flask(__name__)
+JOBS = [{
+  'id': 1,
+  'title': 'Data-analyst',
+  'location': 'Bengaluru,India',
+  'salary': 'Rs. 15,00000'
+}, {
+  'id': 2,
+  'title': 'Data Scientist',
+  'location': 'Delhi,India',
+  'salary': 'Rs 15,00000'
+}, {
+  'id': 3,
+  'title': 'Front end Engineer',
+  'location': 'Remote',
+  'salary': 'Rs. 14,00000'
+}, {
+  'id': 4,
+  'title': 'Backend Emgineer',
+  'location': 'San francisko',
+  'salary': '$120,000'
+}]
+
 
 @app.route("/")
-def hello_world():
-  return render_template('home.html')
+def hello_jovian():
+  return render_template('home.html', jobs=JOBS, company_name='jovian')
 
-if __name__=='__main__':
+
+@app.route("/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+
+
+if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
